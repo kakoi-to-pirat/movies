f5493275fcd020cd3683d87b0c434f065c923fd4
Какой-то Пират <kakoi-to.pirat@yandex.ru>
2018-07-02 10:32:54 +0300

    Update TagsInput.vue

--- resources/assets/js/components/TagsInput.vue
+++ resources/assets/js/components/TagsInput.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <vue-tags-input
-			v-model="tag"
-			:tags="tags"
-			:autocomplete-items="filteredItems"
-	>
+      v-model="tag"
+      :tags="tags"
+      :autocomplete-items="filteredItems">
     </vue-tags-input>
   </div>
 </template>
@@ -13,89 +12,89 @@
 import VueTagsInput from '@johmun/vue-tags-input'
 
 export default {
-	components: {
-		VueTagsInput,
-	},
-	props: [
+  components: {
+    VueTagsInput,
+  },
+  props: [
 '		filmId'
-	],
-	data () {
-		return {
-			tag: '',
-			tags: [],
-			autocompleteItems: [{
-				text: 'Исторический',
-				}, {
-				text: 'Комедия',
-				}, {
-				text: 'Ужасы',
-				}, {
-				text: 'Фантастика',
-				}, {
-				text: 'Боевик',
-				}, {
-				text: 'Вестерн',
-				}, {
-				text: 'Детектив',
-				}, {
-				text: 'Приключения',
-				}, {
-				text: 'Драмма',
-				}, {
-				text: 'Криминал',
-				}, {
-				text: 'Короткометражный',
-				}, {
-				text: 'Детский',
-				}, {
-				text: 'Спорт',
-				}, {
-				text: 'Семейный',
-				}, {
-				text: 'Научный',
-				}, {
-				text: 'Эротика',
-				}, {
-				text: 'Фэнтези',
-				}, {
-				text: 'Кинокомикс',
-				}, {
-				text: 'Военный',
-				}, {
-				text: 'Биографический',
-				}, {
-				text: 'Мистика',
-				}
-			],
-		}
-	},
-	computed: {
-		filteredItems () {
-			return this.autocompleteItems.filter(i => new RegExp(this.tag, 'i').test(i.text))
-		},
-	},
-	mounted () {
-		this.getTags()
-	},
-	watch: {},
-	methods: {
-	getTags () {
-	axios.post('/film/get-tags', {
-		filmId: this.filmId
-	}).then((responce) => {
-		const arr = responce.data
-		JSON.stringify(arr)
-		this.tags = arr
-	})
-	},
-	updateTags () {
-		axios.post('/film/update-tags', {
-			filmId: this.filmId,
-			tags: this.tags
-		}).then((responce) => {
-			console.log(responce.data)
-		})
-	}
-	},
+  ],
+  data () {
+    return {
+      tag: '',
+      tags: [],
+      autocompleteItems: [{
+        text: 'Исторический',
+        }, {
+        text: 'Комедия',
+        }, {
+        text: 'Ужасы',
+        }, {
+        text: 'Фантастика',
+        }, {
+        text: 'Боевик',
+        }, {
+        text: 'Вестерн',
+        }, {
+        text: 'Детектив',
+        }, {
+        text: 'Приключения',
+        }, {
+        text: 'Драмма',
+        }, {
+        text: 'Криминал',
+        }, {
+        text: 'Короткометражный',
+        }, {
+        text: 'Детский',
+        }, {
+        text: 'Спорт',
+        }, {
+        text: 'Семейный',
+        }, {
+        text: 'Научный',
+        }, {
+        text: 'Эротика',
+        }, {
+        text: 'Фэнтези',
+        }, {
+        text: 'Кинокомикс',
+        }, {
+        text: 'Военный',
+        }, {
+        text: 'Биографический',
+        }, {
+        text: 'Мистика',
+        }
+      ],
+    }
+  },
+  computed: {
+    filteredItems () {
+      return this.autocompleteItems.filter(i => new RegExp(this.tag, 'i').test(i.text))
+    },
+  },
+  mounted () {
+    this.getTags()
+  },
+  watch: {},
+  methods: {
+  getTags () {
+  axios.post('/film/get-tags', {
+    filmId: this.filmId
+  }).then((responce) => {
+    const arr = responce.data
+    JSON.stringify(arr)
+    this.tags = arr
+  })
+  },
+  updateTags () {
+    axios.post('/film/update-tags', {
+      filmId: this.filmId,
+      tags: this.tags
+    }).then((responce) => {
+      console.log(responce.data)
+    })
+  }
+  },
 }
 </script>
