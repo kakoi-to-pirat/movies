8bde6202ca423693e8cabfa6287c02949bde82ea
Какой-то Пират <kakoi-to.pirat@yandex.ru>
2018-07-02 11:20:58 +0300

    Update TagsInput.vue

--- resources/assets/js/components/TagsInput.vue
+++ resources/assets/js/components/TagsInput.vue
@@ -16,56 +16,13 @@ export default {
     VueTagsInput,
   },
   props: [
-'		filmId'
+    'filmId'
   ],
   data () {
     return {
       tag: '',
       tags: [],
-      autocompleteItems: [{
-        text: 'Исторический',
-        }, {
-        text: 'Комедия',
-        }, {
-        text: 'Ужасы',
-        }, {
-        text: 'Фантастика',
-        }, {
-        text: 'Боевик',
-        }, {
-        text: 'Вестерн',
-        }, {
-        text: 'Детектив',
-        }, {
-        text: 'Приключения',
-        }, {
-        text: 'Драмма',
-        }, {
-        text: 'Криминал',
-        }, {
-        text: 'Короткометражный',
-        }, {
-        text: 'Детский',
-        }, {
-        text: 'Спорт',
-        }, {
-        text: 'Семейный',
-        }, {
-        text: 'Научный',
-        }, {
-        text: 'Эротика',
-        }, {
-        text: 'Фэнтези',
-        }, {
-        text: 'Кинокомикс',
-        }, {
-        text: 'Военный',
-        }, {
-        text: 'Биографический',
-        }, {
-        text: 'Мистика',
-        }
-      ],
+      autocompleteItems: [],
     }
   },
   computed: {
@@ -78,23 +35,23 @@ export default {
   },
   watch: {},
   methods: {
-  getTags () {
-  axios.post('/film/get-tags', {
-    filmId: this.filmId
-  }).then((responce) => {
-    const arr = responce.data
-    JSON.stringify(arr)
-    this.tags = arr
-  })
-  },
-  updateTags () {
-    axios.post('/film/update-tags', {
-      filmId: this.filmId,
-      tags: this.tags
-    }).then((responce) => {
-      console.log(responce.data)
-    })
-  }
+    getTags () {
+      axios.post('/film/get-tags', {
+        filmId: this.filmId
+      }).then((responce) => {
+        const arr = responce.data
+        JSON.stringify(arr)
+        this.tags = arr
+      })
+    },
+    updateTags () {
+      axios.post('/film/update-tags', {
+        filmId: this.filmId,
+        tags: this.tags
+      }).then((responce) => {
+        console.log(responce.data)
+      })
+    }
   },
 }
 </script>
